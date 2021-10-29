8be596c937a59a50986fb7b273aacca981ec38a8
Kenneth.wang <Kenneth.wang@yijinin.com>
2021-10-29 14:24:39 +0800

    202110291424

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -13,20 +13,20 @@ func NewRouter() *gin.Engine {
 
 	article := v1.NewArticle()
 	tag := v1.NewTag()
-	apiv1 := r.Group("/api/v1")
+	apiV1 := r.Group("/api/v1")
 	{
-		apiv1.POST("/tags", tag.Create)
-		apiv1.DELETE("/tags:id", tag.Delete)
-		apiv1.PUT("/tags:id", tag.Update)
-		apiv1.PATCH("/tags:id/state", tag.Update)
-		apiv1.GET("/tags", tag.List)
+		apiV1.POST("/tags", tag.Create)
+		apiV1.DELETE("/tags:id", tag.Delete)
+		apiV1.PUT("/tags:id", tag.Update)
+		apiV1.PATCH("/tags:id/state", tag.Update)
+		apiV1.GET("/tags", tag.List)
 
-		apiv1.POST("/articles", article.Create)
-		apiv1.DELETE("/articles:id", article.Delete)
-		apiv1.PUT("/articles:id", article.Update)
-		apiv1.PATCH("/articles:id/state", article.Update)
-		apiv1.GET("/articles:id", article.Get)
-		apiv1.GET("/articles", article.List)
+		apiV1.POST("/articles", article.Create)
+		apiV1.DELETE("/articles:id", article.Delete)
+		apiV1.PUT("/articles:id", article.Update)
+		apiV1.PATCH("/articles:id/state", article.Update)
+		apiV1.GET("/articles:id", article.Get)
+		apiV1.GET("/articles", article.List)
 	}
 
 	return r
